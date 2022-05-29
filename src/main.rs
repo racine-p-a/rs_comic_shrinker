48174f66e41d9e06f88af74f55758a862577bc85
Pierre-Alexandre Racine <pierrealexandreracine@gmail.com>
2022-05-29 22:07:53 +0200

    feat : compression to final archive + cleanup

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,30 @@ use std::{env, fs, io};
 use std::collections::btree_map::Entry;
 use std::fs::File;
 use std::io::{copy, Read, stdin, stdout};
+use std::io::{Seek, Write};
 use std::path::Path;
 use rand::Rng;
 use unrar::Archive;
 use walkdir::{DirEntry, WalkDir};
 use image::*;
 use webp::{Encoder, WebPMemory};
+use zip::result::ZipError;
+use zip::write::FileOptions;
 
+const METHOD_STORED : Option<zip::CompressionMethod> = Some(zip::CompressionMethod::Stored);
+#[cfg(feature = "deflate")]
+const METHOD_DEFLATED : Option<zip::CompressionMethod> = Some(zip::CompressionMethod::Deflated);
+#[cfg(not(feature = "deflate"))]
+const METHOD_DEFLATED : Option<zip::CompressionMethod> = None;
+#[cfg(feature = "bzip2")]
+const METHOD_BZIP2 : Option<zip::CompressionMethod> = Some(zip::CompressionMethod::Bzip2);
+#[cfg(not(feature = "bzip2"))]
+const METHOD_BZIP2 : Option<zip::CompressionMethod> = None;
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "full");
     let accepted_input_file_extensions = ["cb7", "cba", "cbr", "cbt", "cbz"];
+    let accepted_destination_file_extensions = ["cbz"];
 
     let args: Vec<String> = env::args().collect();
     if args.len() < 5 {
@@ -42,14 +55,76 @@ fn main() {
         _ => println!("This archive file is not (yet) supported, sorry."),
     };
 
-    let complete_file_list = get_complete_file_list(temporary_output_folder);
+    // SECOND STEP : IMAGE COMPRESSION
+    let complete_file_list = get_complete_file_list(&temporary_output_folder);
     match args[3].as_str() {
         //"avif" => convert_pictures_to_avif(&complete_file_list, &args[4]),
         //"mozjpg" => convert_pictures_to_mozjpp(&complete_file_list, &args[4]),
         "webp" => convert_pictures_to_webp(&complete_file_list, &args[4]),
         _ => println!("Can't convert pictures to this format : {}", &args[3])
     };
+
+    // THIRD STEP : ARCHIVE CREATION
+    if !accepted_destination_file_extensions.contains(&Path::new(&args[2]).extension().unwrap().to_str().unwrap()) { println!("Output file extension not accepted {:?}", &Path::new(&args[2]).extension().unwrap().to_str().unwrap()); return;}
+    let output_filename = Path::new(&args[2]).file_stem().unwrap().to_str().unwrap();
+    match Path::new(&args[2]).extension().unwrap().to_str().unwrap() {
+        "cbz" => zip_folder(&temporary_output_folder, Path::new(&output_filename).with_extension("cbz").to_str().unwrap()),
+        _ => println!("Can't compress to this format (yet) : {}", Path::new(&args[2]).extension().unwrap().to_str().unwrap())
+    };
 }
+
+fn zip_folder(temporary_output_folder: &String, output_filename: &str) {
+    for &method in [METHOD_STORED, METHOD_DEFLATED, METHOD_BZIP2].iter() {
+        if method.is_none() { continue }
+        doit(&temporary_output_folder.as_str(), &output_filename, method.unwrap());
+    }
+    fs::remove_dir_all(&temporary_output_folder);
+}
+
+fn doit(src_dir: &str, dst_file: &str, method: zip::CompressionMethod) -> zip::result::ZipResult<()> {
+    if !Path::new(src_dir).is_dir() {
+        return Err(ZipError::FileNotFound);
+    }
+    let path = Path::new(dst_file);
+    let file = File::create(&path).unwrap();
+    let walkdir = WalkDir::new(src_dir.to_string());
+    let it = walkdir.into_iter();
+    zip_dir(&mut it.filter_map(|e| e.ok()), src_dir, file, method)?;
+    Ok(())
+}
+
+fn zip_dir<T>(it: &mut dyn Iterator<Item=DirEntry>, prefix: &str, writer: T, method: zip::CompressionMethod)
+              -> zip::result::ZipResult<()>
+    where T: Write+Seek
+{
+    let mut zip = zip::ZipWriter::new(writer);
+    let options = FileOptions::default()
+        .compression_method(method)
+        .unix_permissions(0o755);
+    let mut buffer = Vec::new();
+    for entry in it {
+        let path = entry.path();
+        let name = path.strip_prefix(Path::new(prefix)).unwrap();
+        // Write file or directory explicitly
+        // Some unzip tools unzip files with directory paths correctly, some do not!
+        if path.is_file() {
+            //println!("adding file {:?} as {:?} ...", path, name);
+            zip.start_file_from_path(name, options)?;
+            let mut f = File::open(path)?;
+            f.read_to_end(&mut buffer)?;
+            zip.write_all(&*buffer)?;
+            buffer.clear();
+        } else if name.as_os_str().len() != 0 {
+            // Only if not root! Avoids path spec / warning
+            // and mapname conversion failed error on unzip
+            //println!("adding dir {:?} as {:?} ...", path, name);
+            zip.add_directory_from_path(name, options)?;
+        }
+    }
+    zip.finish()?;
+    Result::Ok(())
+}
+
 
 fn convert_pictures_to_webp(complete_file_list: &Vec<DirEntry>, compression: &String) {
     let extension_to_convert = ["jpeg", "jpg", "png", "webp"];
@@ -89,7 +164,7 @@ fn convert_pictures_to_webp(complete_file_list: &Vec<DirEntry>, compression: &St
     }
 }
 
-fn get_complete_file_list(temporary_output_folder: String) -> Vec<DirEntry> {
+fn get_complete_file_list(temporary_output_folder: &String) -> Vec<DirEntry> {
     let mut file_list:Vec<DirEntry> = Vec::new();
     for e in WalkDir::new(temporary_output_folder).into_iter().filter_map(|e| e.ok()) {
         if e.metadata().unwrap().is_file() {
@@ -145,7 +220,6 @@ fn extract_zip_file(file_to_extract: &String, temporary_output_folder: &String) 
         }
     }
 }
-
 
 fn random_string(taille: i32) -> String {
     const CHARSET: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
